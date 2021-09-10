f5117e775ce410f12ba86147ad79583c08ca375f
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-09-10 13:35:39 -0300

    incluindo links para as outras telas

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import houseBlue from '../../../public/icons/house-blue.svg';
+import houseGray from '../../../public/icons/house-gray.svg';
+import rankingBlue from '../../../public/icons/ranking-blue.svg';
+import rankingGray from '../../../public/icons/ranking-gray.svg';
+import userBlue from '../../../public/icons/user-blue.svg';
+import userGray from '../../../public/icons/user-gray.svg';
 import logoSmall from '../../../public/logo-small.svg';
-import house from '../../../public/icons/house.svg';
-import user from '../../../public/icons/user.svg';
 import styles from './styles.module.scss';
 
 export function Navigation() {
@@ -14,12 +18,35 @@ export function Navigation() {
                     <li>
                         <Link href="/home" passHref>
                             <a>
-                                <Image src={house} alt="Dashboard" />
+                                {window.location.pathname === "/home" ? (
+                                    <Image src={houseBlue} alt="Dashboard" />
+                                ) : (
+                                    <Image src={houseGray} alt="Dashboard" />
+                                )}
                             </a>
                         </Link>
                     </li>
                     <li>
-                        <Image src={user} alt="Usuário" />
+                        <Link href="/ranking" passHref>
+                            <a>
+                                {window.location.pathname === "/ranking" ? (
+                                    <Image src={rankingBlue} alt="Ranking" />
+                                ) : (
+                                    <Image src={rankingGray} alt="Ranking" />
+                                )}
+                            </a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/user" passHref>
+                            <a>
+                                {window.location.pathname === "/user" ? (
+                                    <Image src={userBlue} alt="Usuário" />
+                                ) : (
+                                    <Image src={userGray} alt="Usuário" />
+                                )}
+                            </a>
+                        </Link>
                     </li>
                 </ul>
             </nav>
