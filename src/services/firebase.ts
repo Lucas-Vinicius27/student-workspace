3b212a84dd64cc246ee481a24a8ee2cdde5ba088
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-09-10 19:44:18 -0300

    arquivo que inicializa a conexão com firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import * as firebaseAuth from "firebase/auth";
+import * as firebaseDatabase from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebaseAuth;
+const database = firebaseDatabase;
+
+export { firebase, auth, database };
