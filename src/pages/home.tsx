748e510eb9feff9c62f9b43b15ee2a56329f3e90
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-09-22 14:24:22 -0300

    Corrigindo import e conteúdo da home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,33 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { ChallengeBox } from '../components/ChallengeBox/challengeBox';
-import { CompletedChallenge } from '../components/CompletedChallenges/completedChallenges';
-import { Countdown } from '../components/Countdown/countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CompletedChallenge } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { Navigation } from '../components/Navigation';
-import { Profile } from '../components/Profile/profile';
-import { XpBar } from '../components/XpBar/xpBar';
+import { Profile } from '../components/Profile';
+import { XpBar } from '../components/XpBar';
 import styles from '../styles/pages/home.module.scss';
 
 const Home: NextPage = () => {
   return (
     <main>
-      <div className={styles.container}>
-        <Head>
-          <title>Student Workspace</title>
-        </Head>
-      
-        <XpBar />
-        <section>
-        <div>
-          <Profile />
-          <CompletedChallenge />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-       </section>
-      </div>
-      
-
-
-
+      <Head>
+        <title>Student Workspace</title>
+      </Head>
       <div className={styles.wrapper}>
         <Navigation />
-        <section>
-          <h1>Home</h1>
+        <section className={styles.container}>
+          <XpBar />
+          <div>
+            <div>
+              <Profile />
+              <CompletedChallenge />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </div>
         </section>
       </div>
     </main>
