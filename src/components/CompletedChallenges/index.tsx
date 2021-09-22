7404d12135e76504750f0039acc437071f6194ea
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-09-22 15:01:53 -0300

    Correção da palavra desafio

--- src/components/CompletedChallenges/index.tsx
+++ src/components/CompletedChallenges/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 export function CompletedChallenge() {
     return(
         <div className={styles.completedchallengeContainer}>
-            <span>Desáfios completos</span>
+            <span>Desafios completos</span>
             <span>5</span>
         </div>
     );
