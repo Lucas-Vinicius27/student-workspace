83081bc14215de7a4813adb0ab080a62f0abba29
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-09-22 22:08:36 -0300

    Revert "Adicionando pacoete js-cookie e sua tipagem"
    This reverts commit 19ddb3965bb1f374a877c05644ea80107f4397c8.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,15 @@
 import type { NextPage } from 'next';
-import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
 import Image from 'next/image';
-import logoApple from '../../public/icons/apple-icon.svg';
+import Link from 'next/link';
 import logoGoogle from '../../public/icons/google-icon.svg';
+import logoApple from '../../public/icons/apple-icon.svg';
 import logoFull from '../../public/logo-full.svg';
 import logo from '../../public/logo.svg';
 import { Button } from '../components/Button';
-import { useAuth } from "../hooks/useAuth";
 import styles from '../styles/pages/index.module.scss';
 
 const Index: NextPage = () => {
-  const router = useRouter();
-  const { user, signInWithGoogle } = useAuth();
-
-  async function handleLoginGoogle() {
-    if (!user) {
-      await signInWithGoogle();
-    }
-
-    router.push("/home");
-  }
-
-  async function handleLoginApple() {
-    // if (!user) {
-    //   await signInWithGoogle();
-    // }
-
-    router.push("/home");
-  }
-
   return (
     <main className={styles.container}>
       <Head>
@@ -43,14 +23,22 @@ const Index: NextPage = () => {
           <Image src={logoFull} alt="Student Workspace" />
           <h1>Bem-Vindo(a)</h1>
           <p>Aqui será o lugar onde você poderá maximizar seus estudos!</p>
-          <Button estiloButton="google" onClick={handleLoginGoogle}>
-            <Image src={logoGoogle} alt="Logo do Google" />
-            <span>Faça login com sua conta do Google</span>
-          </Button>
-          <Button estiloButton="apple" onClick={handleLoginApple}>
-            <Image width={24} height={24} src={logoApple} alt="Logo da Apple" />
-            <span>Faça login com sua conta da Apple</span>
-          </Button>
+          <Link href="/home" passHref>
+            <a>
+              <Button estiloButton="google">
+                <Image src={logoGoogle} alt="Logo do Google" />
+                <span>Faça login com sua conta do Google</span>
+              </Button>
+            </a>
+          </Link>
+          <Link href="/home" passHref>
+            <a>
+              <Button estiloButton="apple">
+                <Image width={24} height={24} src={logoApple} alt="Logo da Apple" />
+                <span>Faça login com sua conta da Apple</span>
+              </Button>
+            </a>
+          </Link>
         </div>
       </section>
     </main>
