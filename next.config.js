495bbc3ae478fabd5fbedaec8f183b950b7fcfd3
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-11-17 17:08:19 -0300

    regras de redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/ranking",
+        has: [
+          {
+            type: "cookie",
+            key: "sessao",
+            value: "true"
+          }
+        ],
+        destination: "/home",
+        permanent: false
+      },
+      {
+        source: "/ranking",
+        has: [
+          {
+            type: "cookie",
+            key: "sessao",
+            value: "false"
+          }
+        ],
+        destination: "/",
+        permanent: false
+      },
+      {
+        source: "/home",
+        has: [
+          {
+            type: "cookie",
+            key: "sessao",
+            value: "false"
+          }
+        ],
+        destination: "/",
+        permanent: false
+      }
+    ];
+  },
 }
