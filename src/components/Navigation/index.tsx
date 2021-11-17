dbd8e05253b82052ec0f45dd778458d7602ae162
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-11-17 16:53:31 -0300

    removendo sessao dos cookies

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -17,7 +17,7 @@ export function Navigation() {
 
     async function logout() {
         await signOut(auth).then(() => {
-            Cookies.remove("token");
+            Cookies.remove("sessao");
             alert("Até logo! \u{1F44B}\u{1F601}");
             router.push("/");
         }).catch(error => {
