d68471ddd14f3d598eff12a359589f737979b95c
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-11-17 18:11:40 -0300

    comentando código

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -53,12 +53,12 @@ export function ChallengesContextProvider({ children, ...rest }: ChallengesProvi
         Cookies.set("level", String(level));
         Cookies.set("challengesCompleted", String(challengesCompleted));
 
-        update(ref(database, `users/${user?.id}`), {
-            currentXp,
-            level,
-            challengesCompleted
-        });
-    }, [level, currentXp, challengesCompleted, user?.id])
+        // update(ref(database, `users/${user?.id}`), {
+        //     currentXp,
+        //     level,
+        //     challengesCompleted
+        // });
+    }, [level, currentXp, challengesCompleted])
 
     function levelUp() {
         setLevel(level + 1);
