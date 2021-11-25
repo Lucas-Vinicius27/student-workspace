bcd0180854ceef05c36202f9f7a320316448204a
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-11-25 20:47:07 -0300

    trocando a palavra

--- src/components/LevelUpModal/index.tsx
+++ src/components/LevelUpModal/index.tsx
@@ -10,7 +10,7 @@ export function LevelUpModal() {
         <div className={styles.container}>
             <header>{level}</header>
             <strong>Parabéns</strong>
-            <p>Você alcançou um novo level.</p>
+            <p>Você alcançou um novo nível.</p>
             <button type="button" onClick={closeLevelUpModal}>
                 <img src="icons/close.svg" alt="Fechar modal" />
             </button>
